85b883302c3aaecd3732bd3cdb70c8627cbbe483
Armanul Momin <armanul.momin@gmail.com>
2025-08-14 15:02:18 +1000

    Background Thread in SwiftUI

---
+++ BackgroundThreatSwiftUI/BackgroundThreatSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundThreatSwiftUIApp.swift
+//  BackgroundThreatSwiftUI
+//
+//  Created by Arman on 14/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundThreatSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
